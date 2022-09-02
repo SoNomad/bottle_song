8c0b3d738ac95a9eb04cb9d3f86e5093d73b4e26
SoNomad <nurmuhammad16203@gmail.com>
2022-09-02 14:43:19 +0300

    Task done

---
+++ bottleSong.js
@@ -0,0 +1,30 @@
+const generateSingleStanza = number => {
+    
+    //переменная и его спряжения для первой строки
+    let bottle = 'бутылок'    //значение по умолчанию
+
+    if (number % 10 === 1){   //если количество равно 1
+        bottle = 'бутылка' }  //пишется "бутылка"
+    else if (number % 10 >= 2 && number % 10 <= 4){  //если количество равно от 2 до 4
+        bottle = 'бутылки'}                          //пишется "бутылки"
+
+    //переменная и его спряжения для второй строки
+    let bottleLeft = 'бутылок'  //значение по умолчанию
+
+    if ((number-1) % 10 === 1){   //если количество равно 1 
+        bottleLeft = 'бутылка' }  //пишется "бутылка"
+    else if ((number-1) % 10 >= 2 && (number-1) % 10 <= 4){ //если количество равно от 2 до 4
+        bottleLeft = 'бутылки'}                             //пишется "бутылки"
+    
+
+    console.log(`${number} ${bottle} пива на стене, ${number} ${bottle} пива!`)
+    console.log(`Возьми одну, пусти по кругу, ${number-1} ${bottleLeft} пива на стене!`)
+}
+
+const generateSongText = number => {
+    for (let i = number; i > 0; i--) {
+        generateSingleStanza(i)
+    }
+}
+
+generateSongText(4)
